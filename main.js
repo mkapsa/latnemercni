e5f0e8395ff1daef30dee98eaa25547c464df71e
kapsa11 <55392734+kapsa11@users.noreply.github.com>
2021-10-14 23:59:44 +0200

    minor changes

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let wheat = 0;
+let bread = 0;
 
 function harvestWheat(number) {
     wheat += number;
@@ -6,4 +7,18 @@ function harvestWheat(number) {
     if (wheat > 0) {
         document.getElementById('wheat').innerHTML = "wheat: " + Math.floor(wheat);
     }
+}
+
+function bakeBread(number) {   
+
+    if (bread >= 20 * number) {
+        wheat -= 20 * number;
+        bread += number;
+
+        if (bread > 0){
+            document.getElementById('bread').innerHTML = "bread: " + Math.floor(bread);
+        }
+    }
+    
+    document.getElementById('wheat').innerHTML = "wheat: " + Math.floor(wheat);
 }
