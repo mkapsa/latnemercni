5351ea33fad789d171b087d3616b2e35fea897d1
kapsa11 <55392734+kapsa11@users.noreply.github.com>
2021-10-16 21:40:16 +0200

    mrdka zpicena zasrana nasrat

--- main.js
+++ main.js
@@ -1,8 +1,38 @@
+resources = {
+    food:0,
+    wood:0,
+    stone:0,
+    reed:0
+}
 
+function huntFood(number){
+    resources.food += number;
 
+    if (resources.food > 0) {
+        document.getElementById('food').innerHTML = Math.floor(resources.food);
+    }
+}
 
-window.setInterval(function(){
+function cutWood(number){
+    resources.wood += number;
 
+    if (resources.wood > 0) {
+        document.getElementById('wood').innerHTML = Math.floor(resources.wood);
+    }
+}
 
+function mineStone(number){
+    resources.stone += number;
 
-}, 100);
+    if (resources.stone > 0) {
+        document.getElementById('stone').innerHTML = Math.floor(resources.stone);
+    }
+}
+
+function gatherReed(number){
+    resources.reed += number;
+
+    if (resources.reed > 0) {
+        document.getElementById('reed').innerHTML = Math.floor(resources.reed);
+    }
+}
