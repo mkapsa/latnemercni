bdde050ede3f873b2a0cf6d214131e15dcff6c6f
kapsa11 <55392734+kapsa11@users.noreply.github.com>
2021-10-16 16:48:56 +0200

    added hire farmer option, constant price only (50)

--- main.js
+++ main.js
@@ -1,40 +1,72 @@
-let money = 0;
-let wheat = 0;
-let bread = 0;
+resources = {
+    money:50,
+    wheat:0,
+    bread:0
+    }
+
+population = {
+    farmers:0
+}
+
+document.getElementById("money").innerHTML = "money: " + Math.floor(resources.money);
+
 
 
 function harvestWheat(number) {
-    wheat += number;
+    resources.wheat += number;
 
-    if (wheat > 0) {
-        document.getElementById('wheat').innerHTML = "wheat: " + Math.floor(wheat);
+    if (resources.wheat > 0) {
+        document.getElementById('wheat').innerHTML = "wheat: " + Math.floor(resources.wheat);
     }
 }
 
 function bakeBread(number) {
 
-    if (wheat >= 20 * number) {
-        wheat -= 20 * number;
-        bread += number;
+    if (resources.wheat >= 20 * number) {
+        resources.wheat -= 20 * number;
+        resources.bread += number;
 
-        if (bread > 0){
-            document.getElementById('bread').innerHTML = "bread: " + Math.floor(bread);
+        if (resources.bread > 0){
+            document.getElementById('bread').innerHTML = "bread: " + Math.floor(resources.bread);
         }
     }
 
-    document.getElementById('wheat').innerHTML = "wheat: " + Math.floor(wheat);
+    document.getElementById('wheat').innerHTML = "wheat: " + Math.floor(resources.wheat);
 }
 
 function sellBread(number) {
 
-    if (bread >= number) {
-        bread -= number;
-        money += number * 30;
+    if (resources.bread >= number) {
+        resources.bread -= number;
+        resources.money += number * 30;
 
-        if (money >= 0) {
-            document.getElementById('money').innerHTML = "money: " + Math.floor(money);
+        if (resources.money >= 0) {
+            document.getElementById('money').innerHTML = "money: " + Math.floor(resources.money);
         }
+
+        document.getElementById('bread').innerHTML = "bread: " + Math.floor(resources.bread);
     }
 
-    document.getElementById('bread').innerHTML = "bread: " + Math.floor(bread);
+
 }
+
+function hire(job, number) {
+
+    if(job == 'farmers'){
+        population.farmers += number;
+        resources.money -= number * 50;
+
+        document.getElementById("money").innerHTML = "money: " + Math.floor(resources.money);
+
+
+
+    }
+
+
+}
+
+window.setInterval(function(){
+
+    harvestWheat(population.farmers / 10);
+
+}, 100);
