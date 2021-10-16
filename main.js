da56740728c735453e16b58f5b2c72ac154baa8c
kapsa11 <55392734+kapsa11@users.noreply.github.com>
2021-10-16 13:51:40 +0200

    added bake + sell buttons and functionality

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+let money = 0;
 let wheat = 0;
 let bread = 0;
+
 
 function harvestWheat(number) {
     wheat += number;
@@ -9,9 +11,9 @@ function harvestWheat(number) {
     }
 }
 
-function bakeBread(number) {   
+function bakeBread(number) {
 
-    if (bread >= 20 * number) {
+    if (wheat >= 20 * number) {
         wheat -= 20 * number;
         bread += number;
 
@@ -19,6 +21,20 @@ function bakeBread(number) {
             document.getElementById('bread').innerHTML = "bread: " + Math.floor(bread);
         }
     }
-    
+
     document.getElementById('wheat').innerHTML = "wheat: " + Math.floor(wheat);
+}
+
+function sellBread(number) {
+
+    if (bread >= number) {
+        bread -= number;
+        money += number * 30;
+
+        if (money >= 0) {
+            document.getElementById('money').innerHTML = "money: " + Math.floor(money);
+        }
+    }
+
+    document.getElementById('bread').innerHTML = "bread: " + Math.floor(bread);
 }
