920109bba7adad86547e8e131e331e24a9b1f0a4
mkapsa <55392734+kapsa11@users.noreply.github.com>
2022-03-23 15:42:08 +0100

    put localstorage functions back to main (didn't work)

--- js/main.js
+++ js/main.js
@@ -723,6 +723,53 @@ function showContent(){
     for(let i = 0; i < equipmentKeys; i++){
         byId(equipmentKeys[i] + "-button").hidden = equipmentValues[i]
     }
+}
+
+// saving function (localstorage), fires every minute + can be fired manually (button)
+
+function saveGame(){
+    
+    let gameSave = {
+        resources: resources,
+        resourcesPerSec: resourcesPerSec,
+        storage: storage,
+        population: population,
+        buildings: buildings,
+        equipment: equipment,
+        prices: prices,
+        hidden: hidden,
+        rates: rates
+    }
+
+    localStorage.setItem('gameSaveData', JSON.stringify(gameSave))
+}
+
+// loading function - fires at window.onload
+
+function loadGame(){
+    
+    let savedGame = JSON.parse(localStorage.getItem('gameSaveData'))
+
+    if(typeof savedGame.resources !== undefined) resources = savedGame.resources
+    if(typeof savedGame.storage !== undefined) storage = savedGame.storage
+    if(typeof savedGame.population !== undefined) population = savedGame.population
+    if(typeof savedGame.resourcesPerSec !== undefined) resourcesPerSec = savedGame.resourcesPerSec
+    if(typeof savedGame.buildings !== undefined) buildings = savedGame.buildings
+    if(typeof savedGame.equipment !== undefined) equipment = savedGame.equipment
+    if(typeof savedGame.prices !== undefined) prices = savedGame.prices
+    if(typeof savedGame.hidden !== undefined) hidden = savedGame.hidden
+    if(typeof savedGame.rates !== undefined) rates = savedGame.rates
+
+    updateNumbers();    
+    updatePrices();
+}
 
 
+function resetGame(){
+
+    if(window.confirm("Do you really want to reset the game? Your progress will be lost.")){
+        
+        localStorage.clear()
+        location.reload()
+    }           
 }
