538435174b6ae755c43b32a257188032d5430ec6
mkapsa <55392734+kapsa11@users.noreply.github.com>
2022-03-23 14:19:20 +0100

    split gameloop into separate file

---
+++ js/gameloop.js
@@ -0,0 +1,24 @@
+window.setInterval(function(){
+
+    updateFood(resourcesPerSec.food / 10)
+    updateWood(resourcesPerSec.wood / 10)
+    updateOre(resourcesPerSec.ore / 10)
+    updateIron(resourcesPerSec.iron / 10)
+    updateCoal(resourcesPerSec.coal / 10)
+    updateKnowledge(resourcesPerSec.knowledge / 10)
+
+    displayResourcesPerSec()
+    showContent()
+    updateNumbers()
+    buttonDisabled()
+    
+}, 100);
+ 
+window.setInterval(function(){
+    saveGame();
+}, 60 * 1000);
+
+window.setInterval(function(){
+    checkFood();
+    turnOffEquipment()
+}, 2000);
